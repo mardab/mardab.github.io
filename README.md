f387428016d94527b81fcff55efeb91c88aeeb8b
Marcin Dąbrowski <mardab@zoho.eu>
2020-03-09 15:04:22 +0100

    purple site beta

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mardab.github.io
 3rd attempt at static personal page
+
+based on Mauricio Urraco's minimal-resume theme
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/* sweetScroll load */
+document.addEventListener("DOMContentLoaded", function () {
+  const sweetScroll = new SweetScroll({/* some options */ });
+
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
