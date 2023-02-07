4172aabf53f2a2e7ff4e7545bb35da3bd377b566
Marcin Dąbrowski <mardab@zoho.eu>
2023-02-07 08:41:00 +0100

    Give up on website, switch to jsonresume

--- README.md
+++
@@ -1,4 +0,0 @@
-# mardab.github.io
-3rd attempt at static personal page
-
-based on Mauricio Urraco's minimal-resume theme
--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-/* sweetScroll load */
-document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */ });
-
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
